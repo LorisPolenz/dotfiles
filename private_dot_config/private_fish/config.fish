8b66983e8b77d2fd675894f9feeffa75a785052f
Loris Polenz <contact@lorispolenz.com>
2023-03-02 19:36:49 +0100

    Add fish, omf and ssh config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/lorispolenz/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+thefuck --alias | source
